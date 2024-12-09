b2bdf650ff0d94f3d777f21ab0624551a8920923
TAEUK HAM <jerryham7@naver.com>
2024-12-09 22:32:57 +0900

    Added Local, Remote Description & ICE candidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,13 +7,20 @@ const cameraSelect = document.getElementById("cameras")
 
 let muted = false;
 let cameraOff = false; 
+let roomName;
+
+const call = document.getElementById("call")
+
+call.hidden = true;
 
 let myStream
+let myPeerConnection
 
 async function getCameras(){
     try{
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput")
+        const currentCamera = myStream.getVideoTracks()[0]
         cameras.forEach(camera=>{
             const option = document.createElement("option")
             option.value = camera.deviceId
@@ -31,6 +38,7 @@ async function getCameras(){
 }
 
 async function getMedia(deviceId){
+    // 처음 들어가면 deviceId가 없다. default로 실행되는 config
     const initialConstrains = {
         audio : true, 
         video : {facingMode : "user"}
@@ -54,9 +62,6 @@ async function getMedia(deviceId){
         console.log(e)
     }
 }
-
-// 처음에 default로 실행
-getMedia();
 
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
@@ -90,4 +95,84 @@ async function handleCameraChange(){
     await getMedia(cameraSelect.value);
 }
 
+
+const welcome = document.getElementById("welcome")
+
+async function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input")
+    await initCall()
+    // input.value <= room name
+    socket.emit("join_room", input.value, initCall)
+    roomName = input.value;
+    input.value = ""
+
+}
+
+async function initCall(){
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
 cameraSelect.addEventListener("input", handleCameraChange)
+
+const welcomeForm = welcome.querySelector("form")
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+
+// Step 1. 자신이 만든 방에 누군가 들어오면 offer를 생성해서 자신의 local description을 만들고,
+// 들어온 사람에게 offer를 전송
+socket.on("welcome", async ()=>{
+    const offer = await myPeerConnection.createOffer()
+    myPeerConnection.setLocalDescription(offer)
+    socket.emit("offer", offer, roomName)
+    console.log(offer)
+})
+// Step 2. 방에 입장한 사람은 Step 1에서 전달된 offer를 통해 자신의 remote description을 만든다.
+// 그 후, answer를 만들고 이를 통해 자신의 local description을 만든다.
+// 만든 answer은 방의 생성자에게 전달
+socket.on("offer", async(offer) => {
+    myPeerConnection.setRemoteDescription(offer)
+    const answer = await myPeerConnection.createAnswer()
+    myPeerConnection.setLocalDescription(answer)
+    socket.emit("answer", answer, roomName)
+})
+
+// Step 3. 방의 생성자는 Step 2에서 전달된 answer를 통해 자신의 remote description을 만든다.
+// 이로써 방의 생성자와 참가자 모두에게 각각 local, remote description이 1개씩 생성되었다. 
+socket.on("answer", answer=>{
+    myPeerConnection.setRemoteDescription(answer)
+})
+
+// Receiving ICE candidate
+socket.on("ice", ice =>{
+    myPeerConnection.addIceCandidate(ice)
+})
+
+// RTC 코드
+// 여기가 핵심!
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection()
+    myPeerConnection.addEventListener("icecandidate", handleIce)
+    myPeerConnection.addEventListener("addstream", handleAddStream)
+    myStream
+        .getTracks()
+        .forEach(track => myPeerConnection.addTrack(track, myStream))
+    // peer A가 peer B에게 make offer를 하고
+    // peer B는 받은 offer에 대해서 make answer를 하면 둘 사이에 webRTC connection이 만들어진다. 
+     
+}
+
+function handleIce(data){
+    socket.emit("ice", data.candidate, roomName)
+}
+
+function handleAddStream(data){
+    const peerFace = document.getElementById("peerFace")
+    // console.log("Got a Stream from my Peer!")
+    // console.log("Peer's stream : ", data.stream)
+    // console.log("My stream : ", myStream)
+    peerFace.srcObject = data.stream
+}
