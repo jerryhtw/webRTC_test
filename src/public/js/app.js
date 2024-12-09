ca821ae623d21700a9bf0ea0fb698307e0107ddb
TAEUK HAM <jerryham7@naver.com>
2024-12-09 13:56:03 +0900

    project init

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,86 +1,17 @@
 const socket = io();
 
-// socketIO에서는 기본적으로 연결되어 있는 client를 저장하는 map구조가 존재한다. 
+const myFace = document.getElementById("myFace")
 
-const welcome = document.getElementById("welcome")
-const form = document.querySelector("#welcome form");
-const room = document.getElementById("room")
-room.hidden = true;
+let myStream
 
-let roomName;
-
-function addMessage(message){
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li")
-    li.innerText = message
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const msgvalue = input.value;
-    socket.emit("new_message", input.value, roomName, ()=>{
-        addMessage(`You ${msgvalue}`)
-    })   
-    input.value = ""; 
-}
-
-function handleNicknameSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    socket.emit("nickname", input.value);
-}
-
-function handleRoomSubmit(event){
-    event.preventDefault();
-    const input = form.querySelector("input");
-    
-    // emit 1번째 인자 : 즉석에서 "enter_room"이라는 event를 바로 만들어 버릴 수 있다. 
-    // emit 2번째 인자 : socketIO에서는 전송할때 꼭 objecy를 string으로 바꾸지 않고 emit 함수를 통해 바로 object를 보낼 수 있다. 
-    // emit 3번째 인자 : callback 함수를 보낼 수 있다. 헷갈릴 수 있지만, 이 함수는 server 쪽에서 실행되는 것이 아닌 server와 연결된 client에서 실행되는 것. server쪽에서는 이 callback 함수를 받아서 client 쪽에서 어떻게 이 함수를 실행하는지 설정할 수 있다.
-    socket.emit("enter_room", input.value, ()=>{
-        console.log("server is done!");
-        welcome.hidden = true;
-        room.hidden = false;    
-        const h3 = room.querySelector("h3");
-        h3.innerText = `Room ${roomName}`;
-        const msgform = room.querySelector("#msg")
-        const nameform = room.querySelector("#name")
-        msgform.addEventListener("submit", handleMessageSubmit)
-        nameform.addEventListener("submit", handleNicknameSubmit)
-    });
-    roomName = input.value;
-    input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome", (user, newCount)=>{
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;    
-    addMessage(`${user} has joined!`)
-})
-
-socket.on("bye", (user, newCount)=>{ 
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;             
-    addMessage(`${user} has left!`)
-})
-
-socket.on("new_message", (msg)=>{
-    addMessage(msg)
-})
-
-socket.on("room_change", (rooms)=>{
-    if(rooms.length === 0){
-        roomList.innerHTML = "";
-        return;
+async function getMedia(){
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio : true,
+            video : true
+        })
+        console.log(myStream)
+    }catch(e){
+        console.log(e)
     }
-    const roomList = welcome.querySelector("ul")
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    });
-})
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
-import {Server} from "socket.io";
-import {instrument} from "@socket.io/admin-ui";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -14,71 +13,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) =>res.render("home"));
 app.get("/*", (req,res) =>res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-    cors : {
-        origin : ["https://admin.socket.io"],
-        credentials : true
-}});
-
-instrument(wsServer, {
-    auth : false,
-});
-
-
-function publicRooms(){
-    const {
-        sockets : {
-            adapter : {sids, rooms},
-        }
-    } = wsServer;
-
-    const publicRooms = [];
-    rooms.forEach((_, key)=>{
-        if(sids.get(key) === undefined){
-            publicRooms.push(key);
-        }
-    })
-    return publicRooms;
-}
-
-function countRoom(roomname){
-    return wsServer.sockets.adapter.rooms.get(roomname)?.size;
-}
-
-wsServer.on("connection", socket=>{
-    socket["nickname"] = "Anonymous"
-    // app.js에서 만든 "enter_room" 이벤트에 대해 반응
-    socket.on("enter_room", (roomName, callback)=> {
-        // console.log(roomName);
-        // join을 통해 roomName 이름의 room에 입장
-        socket.join(roomName);
-        // socket.to => 특정 room에 msg 보내기
-        // socket.leave => 특정 room 나가기
-        // socket.id => client socket의 고유 id를 알 수 있음
-        // socketsJoin => 특정 client socket을 강제로 특정 room에 join시킬 수 있음. 
-
-        callback();
-
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-        
-    });
-
-    socket.on("disconnecting", ()=>{
-        socket.rooms.forEach(room => {
-            socket.to(room).emit("bye", socket.nickname, countRoom(room)-1 )
-        });
-    })
-
-    socket.on("new_message", (msg, roomName, callback)=>{
-        socket.to(roomName).emit("new_message", `${socket.nickname} : ${msg}`)
-        callback()
-    })
-    socket.on("nickname", nickname =>socket["nickname"] = nickname);    
-});
-
+const wsServer = SocketIO(httpServer);
 
 httpServer.listen(3000, handleListen);
 
